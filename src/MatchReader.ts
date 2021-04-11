23b6099f62a4def5ce17917fd20660cbc6cfed0d
박도희 <dohee.pa@gmail.com>
2021-04-12 08:03:31 +0900

    126. A Composition-Based Approach

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import { dataStringToDate } from './Utils';
+import { dateStringToDate } from './Utils';
 import { MatchResult } from './MatchResult';
+import { MatchData } from './MatchData';
 
 interface DataReader {
   read(): void;
@@ -8,17 +8,24 @@ interface DataReader {
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
+
   constructor(public reader: DataReader) { }
 
-  read(): void {
-    this.data = fs.readFileSync(this.filename, {
-      encoding: 'utf-8'
-    })
-      .split('\n')
-      .map(
-        (row: string): string[] => {
-          return row.split(',');
-        }
-      );
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ];
+      }
+    );
   }
 }
